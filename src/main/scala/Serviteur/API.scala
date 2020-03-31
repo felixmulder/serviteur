0810c80d57b39ff2f3ebdbcc6a8d65a2fe296b30
Felix Mulder <felix.mulder@gmail.com>
2020-04-01 00:48:59 +0200

    Add some docs

--- src/main/scala/Serviteur/API.scala
+++ src/main/scala/Serviteur/API.scala
@@ -1,24 +1,55 @@
 package Serviteur.API
 
+/** A binder for APIs, used as a separator same as Servant's operator */
 case class :>[+A, +B]()
 
+/** A type representing a path parameter */
 final case class PathParam[A]()
+
+/** A type representing a request body */
 final case class RequestBody[A]()
 
+/** A type representing that the endpoint is a `GET` */
 final case class GET()
+
+/** A type representing that the CREATED 201 status will be returned along with
+ *  the `ContentType` and `ResponseBody`
+ */
 final case class CREATED[ContentTypes, ResponseBody]()
+
+/** A type representing that the OK 200 status will be returned along with
+ *  the `ContentType` and `ResponseBody`
+ */
 final case class OK[ContentTypes, ResponseBody]()
+
+/** A type representing the `application/json` content type header */
 final case class JSON()
 
+/** A type function that reduces an `API` into a function
+ *
+ *  As an example:
+ *
+ *  ```
+ *  type GetPony = "pony" :> PathParam[PonyId] :> OK[JSON, Option[Pony]]
+ *
+ *  def getPony: Handler[GetPony] =
+ *    (id: PonyId) => IO {
+ *      db.getPony(id)
+ *    }
+ *  ```
+ */
 type Handler[API] = Handler.Go[API]
 
-private[API] object Handler {
+private[API] object Handler:
+
+  // Starter for Handler reduction:
   type Go[API] = API match
     case prev :> last =>
       HandlerAux[prev, HandlerSingle[last]]
     case _ =>
       HandlerSingle[API]
 
+  // Reduce an `X` known to be a non `:>` type
   type HandlerSingle[X] = X match
     case CREATED[_, response] =>
       IO[response]
@@ -29,6 +60,8 @@ private[API] object Handler {
     case RequestBody[body] =>
       body
 
+
+  // Reduce `API` into a function, keeping `Next` as the result type
   type HandlerAux[API, Next] = API match
     case prev :> last =>
       last match
@@ -46,7 +79,6 @@ private[API] object Handler {
       param => Next
     case RequestBody[body] =>
       body => Next
-}
 
 class IO[A](unsafePerformIO: () => A)
 
